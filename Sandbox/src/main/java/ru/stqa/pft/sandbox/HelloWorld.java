00458c2b2517c5a0b11364b7cd7e5535b1c45d83
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-09-24 15:02:40 +0700

    Function for calculation the distance has been created

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -5,23 +5,25 @@ public class HelloWorld {
     public static void main(String[] args) {
         hello("guys");
         hello("Tanya");
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + "= " + area(a,b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point(4, 6);
+        Point p2 = new Point(2, 3);
+        System.out.println("Расстояние между точками с координатами " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " = " + distance(p1, p2));
     }
 
-    public static void hello(String somebody) {
-        System.out.println("Hello, " + somebody + "!");
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.y - p2.y) * (p1.y - p2.y) + (p1.x - p2.x) * (p1.x - p2.x));
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
+    public static void hello (String somebody) {
+            System.out.println("Hello, " + somebody + "!");
+        }
 
-    public static double area(double a, double b) {
-        return a * b;
     }
-}
