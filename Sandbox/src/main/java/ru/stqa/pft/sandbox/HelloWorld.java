16d65450447ff91cd93afb02ad31e62cdb0586ca
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-09-25 10:24:33 +0700

    Method "distance" was adedd to class "Point"

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -14,12 +14,7 @@ public class HelloWorld {
 
         Point p1 = new Point(4, 6);
         Point p2 = new Point(2, 3);
-        System.out.println("Расстояние между точками с координатами " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " = " + distance(p1, p2));
-    }
-
-
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p1.y - p2.y) * (p1.y - p2.y) + (p1.x - p2.x) * (p1.x - p2.x));
+        System.out.println("Расстояние между точками с координатами " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " = " + p1.distance(p2));
     }
 
     public static void hello (String somebody) {
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,6 +9,10 @@ public class Point {
         this.y = y;
     }
 
+    public double distance(Point p2) {
+        return Math.sqrt((this.y - p2.y) * (this.y - p2.y) + (this.x - p2.x) * (this.x - p2.x));
+    }
+
     }
 
 
