3cbd832bbfbadeb30e126d073c9aecb3ddfd34b6
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-09-28 16:14:57 +0700

    "Address Creation test" was added to project

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddressCreationTest.java
@@ -0,0 +1,83 @@
+package ru.stqa.pft.addressbook;
+
+
+import org.testng.annotations.*;
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddressCreationTest {
+    private WebDriver wd;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.get("http://localhost/addressbook/index.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.id("LoginForm")).click();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testAddressCreation() throws Exception {
+        gotoAddNewPage();
+        fiiAddressForm(new AddressData("First name", "Middle name", "Last name", "Nickname", "title", "company", "address", "123456", "123456", "123456", "123456", "q@q.ru", "q1@q.ru", "q3@q.ru", "homepage", "1", "February", "1990", "6", "January", "2010", "address 2", "123678", "text"));
+        submitAddressCreation();
+        openHomePage();
+
+    }
+
+    private void openHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitAddressCreation() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fiiAddressForm(AddressData addressData) {
+        wd.findElement(By.name("firstname")).sendKeys(addressData.getFirstName());
+        wd.findElement(By.name("middlename")).sendKeys(addressData.getMiddleName());
+        wd.findElement(By.name("lastname")).sendKeys(addressData.getLastName());
+        wd.findElement(By.name("nickname")).sendKeys(addressData.getNickname());
+        //wd.findElement(By.name("photo")).sendKeys("Users/tatiana_moskvina/Desktop/День рождения Жени 2020 уже на хард)\DSCF4588.jpg");
+        wd.findElement(By.name("title")).sendKeys(addressData.getTitle());
+        wd.findElement(By.name("company")).sendKeys(addressData.getCompany());
+        wd.findElement(By.name("address")).sendKeys(addressData.getAddress());
+        wd.findElement(By.name("home")).sendKeys(addressData.getHomeNumber());
+        wd.findElement(By.name("mobile")).sendKeys(addressData.getMobileNumber());
+        wd.findElement(By.name("work")).sendKeys(addressData.getWorkNumber());
+        wd.findElement(By.name("fax")).sendKeys(addressData.getFaxNumber());
+        wd.findElement(By.name("email")).sendKeys(addressData.getEmail());
+        wd.findElement(By.name("email2")).sendKeys(addressData.getSecondEmail());
+        wd.findElement(By.name("email3")).sendKeys(addressData.getThirdEmail());
+        wd.findElement(By.name("homepage")).sendKeys(addressData.getHomepage());
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(addressData.getbDay());
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(addressData.getbMonth());
+        wd.findElement(By.name("byear")).sendKeys(addressData.getbYear());
+        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(addressData.getAday());
+        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(addressData.getAmonth());
+        wd.findElement(By.name("ayear")).sendKeys(addressData.getAyear());
+        wd.findElement(By.name("address2")).sendKeys(addressData.getAddress2());
+        wd.findElement(By.name("phone2")).sendKeys(addressData.getPhone2());
+        wd.findElement(By.name("notes")).sendKeys(addressData.getNotes());
+    }
+
+    private void gotoAddNewPage() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.findElement(By.linkText("Logout")).click();
+        wd.quit();
+    }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddressData.java
@@ -0,0 +1,151 @@
+package ru.stqa.pft.addressbook;
+
+public class AddressData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String homeNumber;
+    private final String mobileNumber;
+    private final String workNumber;
+    private final String faxNumber;
+    private final String email;
+    private final String secondEmail;
+    private final String thirdEmail;
+    private final String homepage;
+    private final String bDay;
+    private final String bMonth;
+    private final String bYear;
+    private final String aday;
+    private final String amonth;
+    private final String ayear;
+    private final String address2;
+    private final String phone2;
+    private final String notes;
+
+    public AddressData(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String homeNumber, String mobileNumber, String workNumber, String faxNumber, String email, String secondEmail, String thirdEmail, String homepage, String bDay, String bMonth, String bYear, String aday, String amonth, String ayear, String address2, String phone2, String notes) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.homeNumber = homeNumber;
+        this.mobileNumber = mobileNumber;
+        this.workNumber = workNumber;
+        this.faxNumber = faxNumber;
+        this.email = email;
+        this.secondEmail = secondEmail;
+        this.thirdEmail = thirdEmail;
+        this.homepage = homepage;
+        this.bDay = bDay;
+        this.bMonth = bMonth;
+        this.bYear = bYear;
+        this.aday = aday;
+        this.amonth = amonth;
+        this.ayear = ayear;
+        this.address2 = address2;
+        this.phone2 = phone2;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomeNumber() {
+        return homeNumber;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public String getWorkNumber() {
+        return workNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSecondEmail() {
+        return secondEmail;
+    }
+
+    public String getThirdEmail() {
+        return thirdEmail;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getbDay() {
+        return bDay;
+    }
+
+    public String getbMonth() {
+        return bMonth;
+    }
+
+    public String getbYear() {
+        return bYear;
+    }
+
+    public String getAday() {
+        return aday;
+    }
+
+    public String getAmonth() {
+        return amonth;
+    }
+
+    public String getAyear() {
+        return ayear;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
