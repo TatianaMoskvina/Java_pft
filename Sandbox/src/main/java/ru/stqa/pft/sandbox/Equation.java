2a611f718fa4fa1948eade649bba84a717ba4017
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-10-05 15:18:28 +0700

    Running tests in different browsers

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -13,20 +13,29 @@ public class Equation {
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else n = 0;
+            } else n = 1;
 
-        if (d > 0) {
-            n = 2;
         } else {
-            if (d == 0) {
+            if (d > 0) {
+                n = 2;
+            } else if (d == 0) {
                 n = 1;
             } else {
                 n = 0;
             }
-        }
 
+
+        }
     }
+
+
 
     public int rootNumber() {
         return n;
