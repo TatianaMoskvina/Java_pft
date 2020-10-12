71b5d4c0f8b47263672d440d8fdeb83385408fc5
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-10-12 15:32:07 +0700

    Added conditions for navigation

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,7 +10,11 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
-        click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))) {
+            return;
+        } click(By.linkText("groups"));
     }
 
     public void gotoAddNewPage() {
@@ -18,6 +22,9 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void openHomePage() {
-        click(By.linkText("home page"));
+        if (isElementPresent(By.id("maintable"))) {
+            return;
+        }
+        click(By.linkText("home"));
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddressModificationTest.java
@@ -8,7 +8,7 @@ public class AddressModificationTest extends TestBase{
 
     @Test
     public void testAddressModification() {
-        //app.getNavigationHelper().openHomePage();
+        app.getNavigationHelper().openHomePage();
         app.getAddressHelper().selectAddress();
         app.getAddressHelper().initAddressModification();
         app.getAddressHelper().fillAddressForm(new AddressData("First name (edited)", "Middle name(edited)", "Last name(edited)", "Nickname", "title", "company", "address", "123456", "123456", "123456", "123456", "q@q.ru", "q1@q.ru", "q3@q.ru", "homepage", "1", "February", "1990", "6", "January", "2010",null ,"address 2", "123678", "text"), false);
