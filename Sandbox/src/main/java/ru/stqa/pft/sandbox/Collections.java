53edaed7db62a8550ff0b89cba61574c4e13e40c
Tatiana Moskvina <moskvina.tania.vit@gmail.com>
2020-10-15 11:41:10 +0700

    Lecture 4.2 collections

---
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Collections {
+    public static void main(String[] args) {
+        String[] langs = {"java", "C#", "Python", "PHP"};
+
+        for (String l: langs) {
+            System.out.println("Я хочу выучить " + l);
+        }
+
+    }
+}
